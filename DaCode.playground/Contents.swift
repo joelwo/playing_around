5ec8557548cc67b8e675280cf528e305a62ab454
Joel  Wohlstedt <joel@Joels-iMac-3.local>
2016-09-18 10:42:51 +0200

    Learning and playing around

---
+++ DaCode.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+
+var age = 44
+var name = "Joel Wohlstedt"
+var year = 2016
+var weight = 96.5
+
+"Joel".uppercased()
+"Joel".lowercased()
+"My name is \(name) and I am \(age) years old."
+
+
+age += 1
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let sampleString: String = "hello"
+let sampleArray: Array = [1, 2, 3.1415, 23, 42]
+
